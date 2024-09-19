7f92aa0908e81c0bec16d771b8cdc431364366b6
Nick Perpich <nicholas.perpich@gmail.com>
2024-09-19 14:43:11 -0500

    account for cached images in the image loading component

--- src/OnLoadImage.js
+++ src/OnLoadImage.js
@@ -6,7 +6,6 @@ export default function OnLoadImage({ src, className = '' }) {
   const [loaded, setLoaded] = useState(false);
 
   const onImageLoaded = () => {
-    // alert('loaded');
     setLoaded(true);
   };
 
@@ -14,7 +13,15 @@ export default function OnLoadImage({ src, className = '' }) {
     const imgElCurrent = imgEl.current;
 
     if (imgElCurrent) {
-      imgElCurrent.addEventListener('load', onImageLoaded);
+      // Check if the image is already loaded (cached)
+      if (imgElCurrent.complete) {
+        onImageLoaded(); // Manually trigger the image loaded callback
+      } else {
+        // Add load event listener for non-cached images
+        imgElCurrent.addEventListener('load', onImageLoaded);
+      }
+
+      // Cleanup event listener when component unmounts
       return () => imgElCurrent.removeEventListener('load', onImageLoaded);
     }
   }, [imgEl]);
@@ -23,12 +30,46 @@ export default function OnLoadImage({ src, className = '' }) {
     <>
       <img
         ref={imgEl}
-        //   style={loaded ? {} : { visibility: 'hidden' }}
         style={loaded ? {} : { display: 'none' }}
         src={src}
         className={`${className} just-loaded`}
-      ></img>
+      />
       {!loaded && <div className="portrait"></div>}
     </>
   );
 }
+
+// import { useEffect, useRef, useState } from 'react';
+// import './App.css';
+
+// export default function OnLoadImage({ src, className = '' }) {
+//   const imgEl = useRef(null);
+//   const [loaded, setLoaded] = useState(false);
+
+//   const onImageLoaded = () => {
+//     // alert('loaded');
+//     setLoaded(true);
+//   };
+
+//   useEffect(() => {
+//     const imgElCurrent = imgEl.current;
+
+//     if (imgElCurrent) {
+//       imgElCurrent.addEventListener('load', onImageLoaded);
+//       return () => imgElCurrent.removeEventListener('load', onImageLoaded);
+//     }
+//   }, [imgEl]);
+
+//   return (
+//     <>
+//       <img
+//         ref={imgEl}
+//         //   style={loaded ? {} : { visibility: 'hidden' }}
+//         style={loaded ? {} : { display: 'none' }}
+//         src={src}
+//         className={`${className} just-loaded`}
+//       ></img>
+//       {!loaded && <div className="portrait"></div>}
+//     </>
+//   );
+// }
